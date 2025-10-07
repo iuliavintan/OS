182d753a424e8e05363a1379916142af893852f9
swaggermf <vlad.simion62@gmail.com>
2025-10-07 15:37:21 +0300

    stage2 in progress, disabled interrupts and setup ds=cs

--- stage1.asm
+++ stage1.asm
@@ -5,82 +5,38 @@ BITS 16
 ;cs = code segment; ds- data segment; es-extended data segment, ss- stack segment, sp - stack pointer, bp- base pointer, ip- instruction pointer
 ;si - index
 ;segment:offset
-cli         ;Disable interrupts while setting up segments
-mov ax, cs
-mov ds, ax   ;Aligns DS with CS so that code and data are in the same segment
-xor ax, ax   
-mov ss, ax   
-mov sp, 0x7C00 ;Set stack pointer to top of bootloader memory
-sti         ;Enable interrupts
-mov [BootDrive], dl ;Store boot drive for later use
+start:
+    cli         ;Disable interrupts while setting up segments
+    mov ax, cs
+    mov ds, ax   ;Aligns DS with CS so that code and data are in the same segment
+    xor ax, ax   
+    mov ss, ax   
+    mov sp, 0x7C00 ;Set stack pointer to top of bootloader memory
+    sti         ;Enable interrupts
+    mov [BootDrive], dl ;Store boot drive for later use
 
 
 ; --- disk read setup ---
-mov si, dap ; point to disk address packet structure
-mov ah, 0x42            ; read sectors using LBA
-mov dl, [BootDrive]            ; first hard disk
-int 0x13
+disk_read:
+    mov si, dap ; point to disk address packet structure
+    mov ah, 0x42            ; read sectors using LBA
+    mov dl, [BootDrive]            ; first hard disk
+    int 0x13
+    jc .disk_error          ; jump if error
+    jmp read_success
 
-jc .disk_error          ; jump if error
-jmp read_success
-
-
-; ; CF=1 → error: AH has status
-; push ax               ; save AX
-; push dx
-; push ds
-; push cs               ; DS=CS so strings/HEX_OUTPUT work
-; pop ds
-; mov dx, ax            ; AH=error code (high), AL undefined
-; call print_hex        ; show 0x00AH
-; mov bx, DISK_ERR_MSG
-; call print_string
-; pop ds
-; pop dx
-; pop ax
-; jmp $
 .disk_error:
     mov bx, DISK_ERR_MSG 
     call print_string  
     jmp $
 
+
+; ; --- jump to loaded code ---
+
 read_success:
-; --- dump memory content ---
-;dump 16 bytes at 0x1000 for debugging
-mov ax, 0x0000
-mov es, ax
-mov di, 0x1000
-mov cx, 16
+    jmp 0x0000:0x1000 ; jump to loaded code (segment:offset)
 
-
-; assumes DS points to the segment of the memory you want to dump
-; SI = starting offset (e.g. 0x1000)
-; CX = number of bytes to dump
-
-dump_loop:
-    mov al, [es:di]
-    inc di          ; AL = [ES:DI], DI++
-    xor dh, dh
-    mov dl, al            ; DX = 00AL
-    call print_hex        ; fills HEX_OUTPUT with "0x??"
-    mov bx, HEX_OUTPUT
-    call print_string     ; print the buffer
-
-    ; print a space
-    mov ah, 0x0E
-    mov al, ' '
-    int 0x10
-
-    loop dump_loop
-    ret
-
-
-; --- jump to loaded code ---
-
-jmp 0x0000:1000 ;Jump to loaded code at 0000:1000 (physical 0x1000)
-
-
-;------------ data ------------ ;
+;------ data ------------ ;
 STATUS_MSG db "Am ajuns aici!", 0
 HEX_OUTPUT db '0x0000', 0 ; buffer for print_hex output
 BootDrive db 0 ; to store boot drive (0x80 for first HDD, 0x00 for floppy)
@@ -102,5 +58,4 @@ dap:
 ; ------------ boot sector padding ------------
 times 510-($-$$) db 0
 dw 0xAA55
-
 
--- stage2.asm
+++ stage2.asm
@@ -1,5 +1,28 @@
 [BITS 16]
-org 0x1000               ; stage-1 will load us at 0000:1000
+[org 0x1000]               ; stage-1 will load us at 0000:1000
 
-signature db "S20K!",0
+start:
+    cli
+    mov ax, cs
+    mov ds, ax
+    sti
+
+    ; Print status message
+    mov bx, STATUS_MSG
+    call print_string
+
+    ; Print hex value of current segment (for verification)
+    mov ax, cs
+    call print_hex
+
+    ; Infinite loop to halt execution
+    jmp $
+
+;---data----
+    STATUS_MSG db "Stage 2 alive!", 0
+    HEX_OUTPUT db '0x0000', 0 ; buffer for print_hex output  
+
+; ------------ includes ------------
+    %include "print_string.asm"
+    %include "print_hex.asm"  
 times 512-($-$$) db 0
