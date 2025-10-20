aec315264ae78889c2a7e2b89e963e4492f9c3a1
vladsimion <vlad@simi>
2025-10-20 14:56:33 +0300

    progress

--- src/idt.c
+++ src/idt.c
@@ -16,6 +16,21 @@ void init_idt() {
 
     // Here you would typically set up the IDT gates using set_idt_gate()
 
+    outPortB(0x20,0x11); //start initialization of PIC
+    outPortB(0xA0,0x11);
+
+    outPortB(0x21,0x20); //remap offset of PIC
+    outPortB(0xA1,0x28);
+
+    outPortB(0x21,0x04); //setup cascading
+    outPortB(0xA1,0x02);
+
+    outPortB(0x21,0x01); //set 8086 mode
+    outPortB(0xA1,0x01);
+
+    outPortB(0x21,0x0); //unmask all interrupts
+    outPortB(0xA1,0x0);
+
     idt_flush((uint32_t)&idt_ptr);
 }
 
