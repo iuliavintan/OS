ae31ecb8c3d8c83237f4ceadcf866aa0fd3e338b
iuliavintan <vintaniulia>
2025-10-20 11:37:32 +0300

    added more colours to vga header

--- src/vga.h
+++ src/vga.h
@@ -4,8 +4,21 @@
 #include"stdint.h"
 
 #define COLOUR8_BLACK 0
+#define COLOUR8_BLUE 1
+#define COLOUR8_GREEN 2
+#define COLOUR8_CYAN 3
+#define COLOUR8_RED 4
 #define COULOUR8_MANGENTA 5
+#define COLOUR8_BROWN 6
 #define COLOUR8_LIGHT_GREY 7    
+#define COLOUR8_DARK_GREY 8
+#define COLOUR8_LIGHT_BLUE 9
+#define COLOUR8_LIGHT_GREEN 10
+#define COLOUR8_LIGHT_CYAN 11
+#define COLOUR8_LIGHT_RED 12
+#define COLOUR8_LIGHT_MANGENTA 13
+#define COLOUR8_LIGHT_BROWN 14
+#define COLOUR8_WHITE 15
 
 #define vga_width 80
 #define vga_height 25
